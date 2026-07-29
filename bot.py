a2b416ebb3bb4854bf02f8734255a89565280685
00mzdq <00mzdq@atomicmail.io>
2026-07-28 22:26:09 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,25 @@
+import os
+from telegram import Update
+from telegram.ext import Application, CommandHandler, ContextTypes
 
+TOKEN = os.getenv("BOT_TOKEN")
+
+if not TOKEN:
+    raise ValueError("BOT_TOKEN تنظیم نشده است")
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(
+        "سلام 👋\n\n"
+        "به ربات ما خوش آمدید.\n\n"
+        "🛒 خرید VPN\n"
+        "📢 اسپانسرها\n"
+        "👤 حساب کاربری"
+    )
+
+def main():
+    app = Application.builder().token(TOKEN).build()
+    app.add_handler(CommandHandler("start", start))
+    app.run_polling()
+
+if __name__ == "__main__":
+    main()
